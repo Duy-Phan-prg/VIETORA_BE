e14e0ae5edc69199bd87ebd8840cf1d76bac854b
Duy-Phan-prg <phanduy.prg@gmail.com>
2026-05-26 05:37:08 +0700

    init backend VIETORA_BE

---
+++ src/main/java/com/example/vietora_be/VietoraBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.vietora_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietoraBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VietoraBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vietora_be/VietoraBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vietora_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietoraBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
